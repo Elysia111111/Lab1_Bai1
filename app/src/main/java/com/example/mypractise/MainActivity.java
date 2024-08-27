6214a2f20c3d1ba74863218eaf9d6ccfd089e098
QUACH HUNG VIET <viet18092004@gmail.com>
2024-08-27 18:05:55 +0700

    Thuc Hanh Lab 1

---
+++ app/src/main/java/com/example/mypractise/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.mypractise;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtX, txtY;
+    TextView txtResult;
+    Button btnPlus, btnMinor, btnMultiply, btnDivide, btnMod;
+    private void innitControl() {
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtResult = findViewById(R.id.txtResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinor = findViewById(R.id.btnMinor);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMod = findViewById(R.id.btnMod);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x+y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        btnMinor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x-y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x*y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x/y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        btnMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x%y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        innitControl();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
